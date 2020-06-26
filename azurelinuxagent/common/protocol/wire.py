f4ae6badd48b545b2a2859201d1c2677ec48200e
larohra <larohra@microsoft.com>
2020-06-26 08:34:38 -0700

    encoding utf8

--- azurelinuxagent/common/protocol/wire.py
+++ azurelinuxagent/common/protocol/wire.py
@@ -1075,7 +1075,7 @@ class WireClient(object):
                        u'<Provider id="{0}">{1}'
                        u'</Provider>'
                        u'</TelemetryData>')
-        data = data_format.format(provider_id, event_str)
+        data = data_format.format(provider_id, event_str).encode('utf-8')
         try:
             header = self.get_header_for_xml_content()
             # NOTE: The call to wireserver requests utf-8 encoding in the headers, but the body should not
