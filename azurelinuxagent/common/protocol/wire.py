13eebdef5d6bc3fb76b9445e0ae61c3621eeb320
larohra <larohra@microsoft.com>
2020-06-29 15:32:50 -0700

    Added debug log

--- azurelinuxagent/common/protocol/wire.py
+++ azurelinuxagent/common/protocol/wire.py
@@ -1122,6 +1122,7 @@ class WireClient(object):
         # Send out all events left in buffer.
         for provider_id in list(buf.keys()):
             if len(buf[provider_id]) > 0:
+                logger.info("No of events this request = {0}".format(events_per_request))
                 self.send_event(provider_id, buf[provider_id])
 
     def report_status_event(self, message, is_success):
