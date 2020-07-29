f3319b5430c83566a2d8d6306a0b0ae313781d1c
larohra <larohra@microsoft.com>
2020-07-28 17:54:48 -0700

    More debug data to script

--- azurelinuxagent/setup_python.sh
+++ azurelinuxagent/setup_python.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
-dir=$(dirname "$(pwd)")
+cur_pwd=$1
+echo "Passed parameter $cur_pwd or $1"
+dir=$(dirname "$cur_pwd")
 echo "Installing python to $dir"
 # echo "cleaning make"
 # make clean
 echo "configuring python"
-"$(pwd)/configure" --prefix="$dir"
+"$cur_pwd/configure" --prefix="$dir"
 echo "create makefile"
 make
 echo "make install"
