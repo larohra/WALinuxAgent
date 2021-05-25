024e9880d75ed9eeca3e656a477a98224173327e
Laveesh Rohra <larohra@microsoft.com>
2021-05-25 11:16:25 -0700

    Release 2.3 (#8)

--- azurelinuxagent/common/version.py
+++ azurelinuxagent/common/version.py
@@ -198,7 +198,7 @@ AGENT_NAME = "WALinuxAgent"
 AGENT_LONG_NAME = "Azure Linux Agent"
 # Setting the version to 9.9.9.9 for testing purposes.
 # Replace this with the actual agent version on release.
-# Current Agent Version = 2.2.54.2
+# Current Agent Version = 2.3.0.2
 AGENT_VERSION = '9.9.9.9'
 AGENT_LONG_VERSION = "{0}-{1}".format(AGENT_NAME, AGENT_VERSION)
 AGENT_DESCRIPTION = """
