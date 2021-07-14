45979ae813ff5766a4f513da6df466ea5252b172
larohra <41490930+larohra@users.noreply.github.com>
2021-07-14 13:10:38 -0700

    always create CPUAccounting file

--- azurelinuxagent/common/cgroupconfigurator.py
+++ azurelinuxagent/common/cgroupconfigurator.py
@@ -297,11 +297,11 @@ class CGroupConfigurator(object):
                 if not os.path.exists(agent_drop_in_file_slice):
                     files_to_create.append((agent_drop_in_file_slice, _AGENT_DROP_IN_FILE_SLICE_CONTENTS))
 
-            if fileutil.findre_in_file(agent_unit_file, r"CPUAccounting=") is not None:
-                CGroupConfigurator._Impl.__cleanup_unit_file(agent_drop_in_file_cpu_accounting)
-            else:
-                if not os.path.exists(agent_drop_in_file_cpu_accounting):
-                    files_to_create.append((agent_drop_in_file_cpu_accounting, _AGENT_DROP_IN_FILE_CPU_ACCOUNTING_CONTENTS))
+            # if fileutil.findre_in_file(agent_unit_file, r"CPUAccounting=") is not None:
+            #     CGroupConfigurator._Impl.__cleanup_unit_file(agent_drop_in_file_cpu_accounting)
+            # else:
+            if not os.path.exists(agent_drop_in_file_cpu_accounting):
+                files_to_create.append((agent_drop_in_file_cpu_accounting, _AGENT_DROP_IN_FILE_CPU_ACCOUNTING_CONTENTS))
 
             if len(files_to_create) > 0:
                 # create the unit files, but if 1 fails remove all and return
